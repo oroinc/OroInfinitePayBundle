9ed70f17694622b35161ec4e5a7f8b86425eadcf
Vova Soroka <vsoroka@oroinc.com>
2018-11-02 18:08:43 +0200

    BB-15268: Add API resource for Customer Address (#20302)
    - add API resource for customer address
    - fix "primary" address handling and validation
    - fix "default" addresses handling and validation
    - add "customRegion" field to all API resources that represent addresses
    - rename "created" and "updated" fields to "createdAt" and "updatedAt" for the following API resources:
      - /api/addresses
      - /api/taxonomies
      - /api/contactaddresses
      - /api/magentoaddresses
      - /api/magentocartaddresses
      - /api/leadaddresses
      - /api/customeruseraddresses
      - /api/orderaddresses
    - rename "frontendOwner" field to "customerUser" for "/api/customeruseraddresses" API resource
    - rename "/api/customer_users" API resource to "/api/customerusers" 
    -  rename "/api/customer_user_addresses" API resource to "/api/customeruseraddresses"
    - rename "/api/customer_user_roles" REST API resource to "/api/customeruserroles"
    - rename "/api/customer_groups" REST API resource to "/api/customergroups"
    - rename "/api/customer_rating" REST API resource to "/api/customerratings" 
    - remove "AbstractAddress", "RequireName" and "RequireRegion" validation groups for addresses, instead of them the appropriate validation constraints are applied for addresses
    - add validation constraint to check that address regionText field can have a value only for countries without predefined regions
    - refactor API functional tests in CustomerBundle
    - add functional tests to validate that "created" and "updated" fields are represented in API as "createdAt" and "updatedAt" 
    - add functional tests to validate that entity aliases for all API resources are valid and consistent
    - add functional tests for "/api/addresses" API resource
    - add functional tests for "/api/contactaddresses" API resource
    - add functional tests for "/api/leadaddresses" API resource

--- Validator/Constraints/CustomerRequireVatIdValidator.php
+++ Validator/Constraints/CustomerRequireVatIdValidator.php
@@ -3,18 +3,19 @@
 namespace Oro\Bundle\InfinitePayBundle\Validator\Constraints;
 
 use Oro\Bundle\AddressBundle\Entity\AddressType;
+use Oro\Bundle\CustomerBundle\Entity\AbstractDefaultTypedAddress;
 use Oro\Bundle\CustomerBundle\Entity\Customer;
-use Oro\Bundle\CustomerBundle\Entity\CustomerAddress;
 use Oro\Bundle\FrontendBundle\Request\FrontendHelper;
 use Oro\Bundle\TaxBundle\Matcher\EuropeanUnionHelper;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
+/**
+ * Validates that VAT is mandatory if a customer has at least one billing address in European Union.
+ */
 class CustomerRequireVatIdValidator extends ConstraintValidator
 {
-    /**
-     * @var FrontendHelper
-     */
+    /** @var FrontendHelper */
     private $frontendHelper;
 
     /**
@@ -26,56 +27,51 @@ class CustomerRequireVatIdValidator extends ConstraintValidator
     }
 
     /**
-     * @param Customer $value
+     * @param Customer   $value
      * @param Constraint $constraint
-     * @throws \InvalidArgumentException
      */
     public function validate($value, Constraint $constraint)
     {
+        if (null === $value) {
+            return;
+        }
+
         if (!$value instanceof Customer) {
-            throw new \InvalidArgumentException(
-                sprintf(
-                    'Value must be instance of "%s", "%s" given',
-                    Customer::class,
-                    is_object($value) ? get_class($value) : gettype($value)
-                )
-            );
+            throw new \InvalidArgumentException(sprintf(
+                'Value must be instance of "%s", "%s" given',
+                Customer::class,
+                is_object($value) ? get_class($value) : gettype($value)
+            ));
         }
 
         if ($this->frontendHelper->isFrontendRequest()) {
             return;
         }
 
-        if ($this->getBillingEUAddesses($value) && empty($value->getVatId())) {
+        if (empty($value->getVatId()) && $this->hasEuropeanUnionBillingAddresses($value)) {
             $this->context->addViolation($constraint->message);
         }
     }
 
     /**
      * @param Customer $customer
-     * @return array|CustomerAddress[]
+     *
+     * @return bool
      */
-    protected function getBillingEUAddesses(Customer $customer)
+    private function hasEuropeanUnionBillingAddresses(Customer $customer): bool
     {
-        $billingEUAddresses = [];
-
+        $result = false;
+        /** @var AbstractDefaultTypedAddress $address */
         foreach ($customer->getAddresses() as $address) {
-            if ($address->isEmpty()
-                || !EuropeanUnionHelper::isEuropeanUnionCountry($address->getCountryIso2())) {
-                continue;
-            }
-
-            $billingType = $address->getTypes()->filter(
-                function (AddressType $addressType) {
-                    return $addressType->getName() === AddressType::TYPE_BILLING;
-                }
-            );
-
-            if (!$billingType->isEmpty()) {
-                $billingEUAddresses[] = $address;
+            if (!$address->isEmpty()
+                && EuropeanUnionHelper::isEuropeanUnionCountry($address->getCountryIso2())
+                && $address->hasTypeWithName(AddressType::TYPE_BILLING)
+            ) {
+                $result = true;
+                break;
             }
         }
 
-        return $billingEUAddresses;
+        return $result;
     }
 }
